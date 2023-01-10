603d1c910d526a316f62ad54d675d3bbfbdf65cd
EC2 Default User <ec2-user@ip-172-30-3-113.ec2.internal>
2023-01-10 14:14:31 +0000

    New charts big update

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from json import dumps
 import logging
 import os
 import requests
+from urllib.parse import urlparse
+
 import keys
 
 from flask import render_template, url_for, request, jsonify
@@ -97,9 +99,19 @@ def info_d3_force():
     return render_template('info/d3_force.html')
 
 
+@app.route('/info/d3_concept')
+def info_d3_concept():
+    return render_template('info/d3_concept.html')
+
+
 @app.route("/info/any_node")
 def get_anychart():
     return render_template("/info/any_node.html")
+
+
+@app.route("/info/any_edge_graph")
+def get_anyedgegraph():
+    return render_template("/info/any_edge_graph.html")
 
 
 @app.route('/dexter/report', methods=['POST',])
@@ -116,8 +128,8 @@ def dexter_report(id=None):
             "WITH p, item "
             "UNWIND [x IN item.links] AS link "
             "MERGE (l:Page {url: link.to}) "
-            "MERGE (p) -[le:LINK_TO]-> (l) "
-            "SET le.redirect = l.redirect "
+            "MERGE (p) -[lt:LINK_TO]-> (l) "
+            "SET lt.redirect = l.redirect "
             "WITH p, item.diagnostics AS diags "
             "UNWIND [x in diags] AS diag "
             "MERGE (d:DIAGNOSTIC "
@@ -179,19 +191,19 @@ def get_nodes():
                     mimetype="application/json")
 
 
-@app.route("/json/any/nodes")
-def get_any_nodes():
+@app.route("/json/d3/concept")
+def get_d3_concept():
     def pages(tx, limit):
         return list(tx.run(
-            "MATCH (node:Page) "
-            "RETURN node.url AS url, node.screenshot AS screenshot "
+            "MATCH (p:Page) "
+            "RETURN p "
             "LIMIT $limit",
             {"limit": limit}
         ))
     def links(tx, limit):
         return list(tx.run(
-            "MATCH (s:Page)-[:LINK_TO]->(d:Page) "
-            "RETURN s.url AS src, d.url AS dest "
+            "MATCH (s:Page)-[l:LINK_TO]->(d:Page) "
+            "RETURN s.url AS src, l AS link, d.url AS dest "
             "LIMIT $limit",
             {"limit": limit}
         ))
@@ -201,138 +213,84 @@ def get_any_nodes():
     nodes = []
     i = 0
     node_id = {}
-    for row in results:
-        #raise Exception(dir(row))
-        node_id[row["url"]] = i
-        node = {"id": i, "url": row["url"], "label": "page",}
-        if row.get("screenshot"):
-            node.update({"group": "screenshot",
-                         "screenshot_storage": row["screenshot"]})
+    for result in results:
+        page = result['p']
+        node_id[page.get("url")] = i
+        node = {"id": i, "url": page.get("url"),
+                "label": page.get("url")[:32],
+                "group": page.get("mimeType")}
+        if not page.get("contentTested"):
+            node.update({"group": "disabled"})
+        if page.get("screenshot"):
+            node.update({ "height": 100, "width": 100,
+                "shape": "square", "stroke": "none",
+                "fill": { "src": page["screenshot"], "mode": "fit" }
+            })
         nodes.append(node)
         i += 1
-    rels = []
+    edges = []
     results = db.read_transaction(links, request.args.get("limit", 10000))
-    for row in results:
-        rels.append({"from": node_id[row["src"]],
-                     "to": node_id[row["dest"]]})
-    return Response(dumps({"nodes": nodes, "edges": rels}),
+    for result in results:
+        edge = {"from": node_id[result["src"]],
+                "to": node_id[result["dest"]]}
+        link = result['link']
+        #XXX bugged? check redirect
+        if link.get("redirect"):
+            edge.update({'stroke': {'dash': "10 5", 'color': '#000000'}})
+        edges.append(edge)
+    return Response(dumps({"nodes": nodes, "edges": edges}),
                     mimetype="application/json")
 
 
-def serialize_movie(movie):
-    return {
-        "id": movie["id"],
-        "title": movie["title"],
-        "summary": movie["summary"],
-        "released": movie["released"],
-        "duration": movie["duration"],
-        "rated": movie["rated"],
-        "tagline": movie["tagline"],
-        "votes": movie.get("votes", 0)
-    }
-
-
-def serialize_cast(cast):
-    return {
-        "name": cast[0],
-        "job": cast[1],
-        "role": cast[2]
-    }
-
-
-@app.route("/graph")
-def get_graph():
-    def work(tx, limit):
+@app.route("/json/any/nodes")
+def get_any_nodes():
+    def pages(tx, limit):
         return list(tx.run(
-            "MATCH (m:Movie)<-[:ACTED_IN]-(a:Person) "
-            "RETURN m.title AS movie, collect(a.name) AS cast "
+            "MATCH (p:Page) "
+            "RETURN p "
+            "LIMIT $limit",
+            {"limit": limit}
+        ))
+    def links(tx, limit):
+        return list(tx.run(
+            "MATCH (s:Page)-[l:LINK_TO]->(d:Page) "
+            "RETURN s.url AS src, l AS link, d.url AS dest "
             "LIMIT $limit",
             {"limit": limit}
         ))
 
     db = get_db()
-    results = db.read_transaction(work, request.args.get("limit", 100))
+    results = db.read_transaction(pages, request.args.get("limit", 10000))
     nodes = []
-    rels = []
     i = 0
-    for record in results:
-        nodes.append({"title": record["movie"], "label": "movie"})
-        target = i
+    node_id = {}
+    for result in results:
+        page = result['p']
+        node_id[page.get("url")] = i
+        node = {"id": i, "url": page.get("url"),
+                "label": page.get("url")[:32],
+                "group": page.get("mimeType")}
+        if not page.get("contentTested"):
+            node.update({"group": "disabled"})
+        if page.get("screenshot"):
+            node.update({ "height": 100, "width": 100,
+                "shape": "square", "stroke": "none",
+                "fill": { "src": page["screenshot"], "mode": "fit" }
+            })
+        nodes.append(node)
         i += 1
-        for name in record["cast"]:
-            actor = {"title": name, "label": "actor"}
-            try:
-                source = nodes.index(actor)
-            except ValueError:
-                nodes.append(actor)
-                source = i
-                i += 1
-            rels.append({"source": source, "target": target})
-    return Response(dumps({"nodes": nodes, "links": rels}),
+    edges = []
+    results = db.read_transaction(links, request.args.get("limit", 10000))
+    for result in results:
+        edge = {"from": node_id[result["src"]],
+                "to": node_id[result["dest"]]}
+        link = result['link']
+        #XXX bugged? check redirect
+        if link.get("redirect"):
+            edge.update({'stroke': {'dash': "10 5", 'color': '#000000'}})
+        edges.append(edge)
+    return Response(dumps({"nodes": nodes, "edges": edges}),
                     mimetype="application/json")
-
-
-@app.route("/search")
-def get_search():
-    def work(tx, q_):
-        return list(tx.run(
-            "MATCH (movie:Movie) "
-            "WHERE toLower(movie.title) CONTAINS toLower($title) "
-            "RETURN movie",
-            {"title": q_}
-        ))
-
-    try:
-        q = request.args["q"]
-    except KeyError:
-        return []
-    else:
-        db = get_db()
-        results = db.read_transaction(work, q)
-        return Response(
-            dumps([serialize_movie(record["movie"]) for record in results]),
-            mimetype="application/json"
-        )
-
-
-@app.route("/movie/<title>")
-def get_movie(title):
-    def work(tx, title_):
-        return tx.run(
-            "MATCH (movie:Movie {title:$title}) "
-            "OPTIONAL MATCH (movie)<-[r]-(person:Person) "
-            "RETURN movie.title as title,"
-            "COLLECT([person.name, "
-            "HEAD(SPLIT(TOLOWER(TYPE(r)), '_')), r.roles]) AS cast "
-            "LIMIT 1",
-            {"title": title_}
-        ).single()
-
-    db = get_db()
-    result = db.read_transaction(work, title)
-
-    return Response(dumps({"title": result["title"],
-                           "cast": [serialize_cast(member)
-                                    for member in result["cast"]]}),
-                    mimetype="application/json")
-
-
-@app.route("/movie/<title>/vote", methods=["POST"])
-def vote_in_movie(title):
-    def work(tx, title_):
-        return tx.run(
-            "MATCH (m:Movie {title: $title}) "
-            "SET m.votes = coalesce(m.votes, 0) + 1;",
-            {"title": title_}
-        ).consume()
-
-    db = get_db()
-    summary = db.write_transaction(work, title)
-    updates = summary.counters.properties_set
-
-    db.close()
-
-    return Response(dumps({"updates": updates}), mimetype="application/json")
 
 
 if __name__ == "__main__":
